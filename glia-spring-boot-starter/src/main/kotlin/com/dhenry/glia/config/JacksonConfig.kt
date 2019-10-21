00d47b5e96e46fdc9d3a5a3ae43185d57b69e6b6
Darnell H <darnelljh@gmail.com>
2019-10-21 00:48:01 -0400

    Register KotlinModule with ObjectMapper

--- glia-spring-boot-starter/src/main/kotlin/com/dhenry/glia/config/JacksonConfig.kt
+++ glia-spring-boot-starter/src/main/kotlin/com/dhenry/glia/config/JacksonConfig.kt
@@ -3,6 +3,7 @@ package com.dhenry.glia.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,6 +15,7 @@ class JacksonConfig {
         return ObjectMapper()
             .registerModule(Jdk8Module())
             .registerModule(JavaTimeModule())
+            .registerModule(KotlinModule())
     }
 
 }
